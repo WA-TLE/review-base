4780388a20132ae291eaea89f35c907baa6abe2f
dy <dyu32345@gmail.com>
2023-11-09 17:51:23 +0800

    初始 review 代码

---
+++ collection/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: dy
+ * @Date: 2023-11-9 17:41:10
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
